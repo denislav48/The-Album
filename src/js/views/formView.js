2201c9462a21464a5685e96642da627dd236d5f8
Denislav <denislavstoyanov48@gmail.com>
2018-10-29 22:42:07 +0200

    removed the excess

--- src/js/views/formView.js
+++
@@ -1,8 +0,0 @@
-import {elements} from './base';
-
-elements.uploadPhotoButton.addEventListener('click', () => {
-    elements.popupForm.style.display = 'block';
-});
-elements.closeForm.addEventListener('click', () => {
-    elements.popupForm.style.display = 'none';
-});
