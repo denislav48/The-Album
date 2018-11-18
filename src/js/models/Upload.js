98920fa9605ab4c3c899796c68e25cc2ad347125
Denislav <denislavstoyanov48@gmail.com>
2018-11-18 23:51:00 +0200

    added file_extension validation

--- src/js/models/Upload.js
+++ src/js/models/Upload.js
@@ -3,47 +3,64 @@ import { elements } from '../views/base';
 
 //take the photo file name
 let selectedFile,
-  selectedFileName;
+  selectedFileName,
+  allowed_extensions = ['jpg', 'jpeg', 'png', 'gif'],
+  file_extension,
+  isAllowed = false;
 elements.uploadFormUploadFile.addEventListener('change', (e) => {
   selectedFile = e.target.files[0];
   selectedFileName = selectedFile.name;
-  console.log(selectedFileName);
+  file_extension = selectedFileName.split('.').pop().toLowerCase();
 });
+
+
 
 export const writeNewPost = (title, username, category) => {
 
-  // Get a key for a new Post.
-  const newPostKey = firebase.database().ref().child('Album').push().key;
+  //Check if the file extension is allowed
+  allowed_extensions.forEach(el => {
+    if (el === file_extension) {
+      isAllowed = true;
+    }
+  });
 
-  const postData = {
-    title: title,
-    date: new Date(),
-    username: username,
-    category: category,
-    key: newPostKey
-  };
+  if (isAllowed) {
 
-  // Writting the new post's data simultaneously in the Album list and in the images post list.
-  
-  
-  let updates = {};
+    // Get a key for a new Post.
+    const newPostKey = firebase.database().ref().child('Album').push().key;
 
-  //Create new post reference key with it's coresponding data
-  updates['Album/' + newPostKey] = postData;
-  
-  //Create new reference key to firebase storage
-  firebase.storage().ref().child('images/' + newPostKey)
-    .put(selectedFile)
-    //get the url of the postet photo
-    .then(() => firebase.storage().ref().child('images/' + newPostKey).getDownloadURL())
-    //add downloadURL property to the post data object
-    .then(snap => {
-      postData['downloadURL'] = snap;
-    })
-    //write postData to firebase database
-    .then(() => {
-      firebase.database().ref().update(updates);
-    })
+    const postData = {
+      title: title,
+      date: new Date(),
+      username: username,
+      category: category,
+      key: newPostKey
+    };
+
+    // Writting the new post's data simultaneously in the Album list and in the images post list.
+
+
+    let updates = {};
+
+    //Create new post reference key with it's coresponding data
+    updates['Album/' + newPostKey] = postData;
+
+    //Create new reference key to firebase storage
+    firebase.storage().ref().child('images/' + newPostKey)
+      .put(selectedFile)
+      //get the url of the postet photo
+      .then(() => firebase.storage().ref().child('images/' + newPostKey).getDownloadURL())
+      //add downloadURL property to the post data object
+      .then(snap => {
+        postData['downloadURL'] = snap;
+      })
+      //write postData to firebase database
+      .then(() => {
+        firebase.database().ref().update(updates);
+      });
+      
+      isAllowed = false;
+  }
 }
 
 
